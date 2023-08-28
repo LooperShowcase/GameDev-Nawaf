849e447b72b70259734214e43f9217fb5b2c4a31
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2023-08-28 14:04:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,81 +1,156 @@
-const cardsContainer = document.getElementById("cards");
-let cards = [];
+const cardsContainer = document.querySelector(".cards");
 let firstCard, secondCard;
+let cards = [];
+let card;
+let score = 0;
 let lockBoard = false;
+let startedTimer = false;
+let ended =false;
+document.querySelector(".Score").textContent = score;
 
 fetch("./data/cards.json")
-.then((res) => res.json())
-.then((data) => {
-    cards = [...data,...data];
-    // first data => "image": "assets/chili.png".(link).
-    // secind data =>  "name": "chili".(name).
+  .then((res) => res.json())
+  .then((data) => {
+    cards = [...data, ...data];
+    shuffleCards();
+    GenerateCards();
     console.log(cards);
-    });
+  });
 
-    function shuffleCards()
-    {
-        let temp;//ward
-        let currentIndex = cards.length;//adam
-        let randomIndex;//john
+function shuffleCards() {
+  let currentIndex = cards.length;
+  let randomIndex;
+  let temporaryValue;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
+  }
+}
 
-        while(currentIndex !== 0)
-        {
-            randomIndex = Math.floor(Math.random() * currentIndex);//Math.floor => close the number to the lower value ====> to the numbers not to be outside the array.
-            currentIndex--;
-            temp = cards[currentIndex];
-            cards[currentIndex] = cards[randomIndex];
-            cards[randomIndex] = temp;
-        }  
+function GenerateCards() {
+  for (card of cards) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.setAttribute("data-name", card.name);
+    cardElement.innerHTML = `
+        <div class="front">
+            <img class="front-image" src=${card.image} />
+        </div>
+        <div class="back"></div>
+        `;
+    cardElement.addEventListener("click", flipCard); // add this
+    cardElement.addEventListener("touchstart", flipCard);
+    cardsContainer.appendChild(cardElement);
+  }
+}
+
+function flipCard() {
+    if (!startedTimer) {
+        startedTimer = true;
+        startTimer();
     }
-    function jenerate_cards() // jenerate and create cards.
-        {
-         for(let card of cards)
-         {
-            const cardElement = document.createElement('div');// const==> like private it doesn't be changed.
-            cardElement.classList.add("card");
-            cardElement.setAttribute("data-name", card.name);
-            cardElement.innerHTML =`
-            <div class = "front">
-            <img class = "front-image" src = ${card.image}/>
-            </div>
-            <div class = "back"></div>
-            `;
-            // this code from HTML =====> to connect html > js.  
-         }
-         cardsContainer.appendChild(cardElement);
-         cardElement.addElementListener("click",flipcard);
-        }
-        function flipcard()
-        {
-            if(lockBoard)
-            {
-                return;
-            }
-            if(this === firstcard)
-            {
-                return;
-            }
-            this.classList.add("fliped");
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-            if(firstCard)
-            {
-                firstcard = this;
-                return;
-            }
-            secondCard = this;
+  this.classList.add("flipped");
+
+  if (!firstCard) {
+    firstCard = this;
+    return;
+    
+  }
+
+  secondCard = this;
+  document.querySelector(".Score").textContent = score;
+  lockBoard = true;
+  checkForMatch(); // add this
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  if (isMatch) {
+    disableCards();
+  } else {
+    unflipCards();
+  }
+if(score === 9)
+{
+    ended = true;
+    startConfetti()
+}
+}
+function unlockBoard() {
+  firstCard = null;
+  secondCard = null;
+  lockBoard = false;
+}
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  firstCard.removeEventListener("touchstart", flipCard);
+  secondCard.removeEventListener("touchstart", flipCard);
+  score = score + 1;
+  unlockBoard();
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove("flipped");
+    secondCard.classList.remove("flipped");
+    unlockBoard();
+  }, 1000);
+}
+
+function restart() {
+  score = 0;
+  unlockBoard();
+  shuffleCards();
+  document.querySelector(".Score").textContent = score;
+  cardsContainer.innerHTML = "";
+  GenerateCards();
+}
+
+function startTimer() {
+    // Set the duration of the timer in seconds
+    var duration = 60;
+
+    // Get the current timestamp
+    var startTime = new Date().getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        if (ended) {
+            return;
+        }
+        // Get the current timestamp
+        var currentTime = new Date().getTime();
+
+        // Calculate the elapsed time in seconds
+        var elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
+
+        // Calculate the remaining seconds
+        var remainingSeconds = duration - elapsedSeconds;
+
+        // Display the remaining seconds in the element with id="demo"
+        document.getElementById("demo").innerHTML = remainingSeconds;
+        console.log(remainingSeconds);
+
+        // If the timer reaches 0, stop the interval and display "EXPIRED"
+        if (remainingSeconds <= 0) {
+            ended = true;
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
             lockBoard = true;
-            checkForMatch();// compare the first card and the second card.
-        } 
-
-        function checkForMatch()// check if the cards are the same .
-        {
-            let isMatch = firstCard.dataset.name === secondCard.dataset.name;
-            if(isMatch)
-            {
-                disableCards();
-            }
-            else
-            {
-                unfliped();
-            }
         }
+    }, 1000);
+}
+function stopTime()
+{
+    if(flipCard == 9)
+    {
+        setTimeout;
+    }
+}
